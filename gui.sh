36b26117ac282a27f39177561fd91cd3d5f1fc0e
John Phillips <profphillips@gmail.com>
2016-08-20 23:29:55 +0000

    AWS Ubuntu 2016.04 Developer Student Server Setup

---
+++ gui.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- UPDATING THE SYSTEM FOR AWS EC2 MULTIUSER STUDENT DEVELOPER UBUNTU 16.04 GUI SERVER'
+echo '---- This is the second part of the setup script and deals with adding an xrdp Mate GUI.'
+echo '---- chmod 700 awscreategui.sh and run as root.'
+echo '---- version 20160820'
+echo '-----------------------------------------------------------------------------------------------'
+
+echo 'Starting shell script at:'
+date
+whoami
+pwd
+
+echo '--'
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- INSTALLING GUI ENVIRONMENT'
+echo '-----------------------------------------------------------------------------------------------'
+
+# To see what software is available use: apt-cache search mate
+# to remove a package use: sudo apt-get purge --auto-remove packagename
+
+# install remote destop server software
+apt-get -qq install -y xrdp
+
+# set Mate to be the default window manager
+sed -i 's/.*\/etc\/X11\/Xsession/mate-session/' /etc/xrdp/startwm.sh
+
+# The following command installs most of what we need for a basic Mate remote desktop system
+# We do get an error with the trash applet but can just delete the applet on first run
+# .. or install mate-desktop-environment below to fix applet problem
+apt-get -qq install -y mate-core
+
+# not sure if the following line is needed
+# recommended by http://c-nergy.be/blog/?p=5874
+apt-get -qq install -y mate-notification-daemon
+
+# mate-utils adds screenshot menu item
+apt-get -qq install -y mate-utils
+
+# mate-menu lets user configure which programs appear on the menu bar
+apt-get -qq install -y mate-menu
+
+# mate-desktop-environment adds screensaver which we do not want -- remove it
+# but we may want some of the other apps it gives us; it also fixes trash applet startup error
+apt-get -qq install -y mate-desktop-environment
+#apt-get -qq install -y mate-desktop-environment-extras
+apt-get purge -y --auto-remove mate-screensaver
+
+# ubuntu-mate-core adds a gig of stuff -- not really needed for remote desktop
+# ubuntu-mate-desktop adds another gig of stuff including LibreOffice -- not needed
+#apt-get -qq install -y ubuntu-mate-core
+#apt-get -qq install -y ubuntu-mate-desktop
+#sed -i 's/.*\/etc\/X11\/Xsession/mate-session/' /etc/xrdp/startwm.sh
+
+# suggest installing the following themes and fonts and then choose a small/simple/quick wallpaper
+# I usually set the mono font to inconsolata medium 14 and the wallpaper to 
+apt-get install -y mate-themes ubuntu-mate-themes
+apt-get install -y ubuntu-mate-wallpapers-utopic ubuntu-mate-wallpapers-vivid
+apt-get install -y fonts-inconsolata fonts-dejavu fonts-droid-fallback fonts-liberation fonts-ubuntu-font-family-console
+
+# add a few useful apps; gdebi needed to install chrome; seemed like I needed xterm for something a while back...
+apt-get install -y xterm gdebi-core
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- ADDING BROWSERS'
+echo '-----------------------------------------------------------------------------------------------'
+
+apt-get install -y firefox
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+gdebi -n google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- ADDING ADVANCED TEXT EDITORS'
+echo '-----------------------------------------------------------------------------------------------'
+
+apt-get -qq install -y pluma vim-gnome
+
+# Had problems with auto install of atom. Manual install of lastest beta also failed 20160820.
+#wget https://github.com/atom/atom/releases/download/v1.9.9/atom-amd64.deb
+#gdebi -n atom-amd64.deb
+
+# This did not work either...
+#add-apt-repository -y ppa:webupd8team/atom
+#apt-get -y update
+#apt-get install -y atom
+
+# Brackets seems to work fine -- nice HTML editor
+wget https://github.com/adobe/brackets/releases/download/release-1.7/Brackets.Release.1.7.64-bit.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/libg/libgcrypt11/libgcrypt11_1.5.3-2ubuntu4_amd64.deb
+gdebi -n libgcrypt11_1.5.3-2ubuntu4_amd64.deb
+gdebi -n Brackets.Release.1.7.64-bit.deb
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- ADDING MISC.'
+echo '-----------------------------------------------------------------------------------------------'
+
+# Had some problems last year with dropbox with minimal install; haven't tried it recently
+#wget https://www.dropbox.com/install
+
+# synaptic is a gui install tool; will need to give user sudo privledges in order to use it.
+apt-get install -y synaptic
+
+# Add graphics to Python3
+apt-get install -y python3-tk
+
+
+#echo '-----------------------------------------------------------------------------------------------'
+#echo '---- INSTALLING Mono/C# GUI IDE'
+#echo '-----------------------------------------------------------------------------------------------'
+
+# the following is a large install; only install this if you need to do C# development in a GUI 
+#    echo '---- INSTALLING C, C++, C# Mono IDE'
+#    apt-get install -y monodevelop
+#    echo '--'
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- INSTALLING Spring Tool Suite / Eclipse GUI IDE'
+echo '-----------------------------------------------------------------------------------------------'
+
+# Spring Tool Suite (sts) is a large install but useful for developing Java software
+# sts seems to be better than the standard ubuntu eclipse install which is a rather old version  (3.8 I believe)
+# sts uses eclipse 4.6 which was released 20160622
+# for better JavaFX support you can do Help/MarketPlace, search on FX, choose/install e(fx)clipse
+
+echo '---- INSTALLING SPRING STS / ECLIPSE IDE 20160819 VERSION'
+wget http://dist.springsource.com/release/STS/3.8.1.RELEASE/dist/e4.6/spring-tool-suite-3.8.1.RELEASE-e4.6-linux-gtk-x86_64.tar.gz
+tar xvzf spring-tool-suite-3.8.1.RELEASE-e4.6-linux-gtk-x86_64.tar.gz
+rm -f spring-tool-suite-3.8.1.RELEASE-e4.6-linux-gtk-x86_64.tar.gz
+mkdir /etc/skel/sts
+cp -r sts-bundle/sts-3.8.1.RELEASE/* /etc/skel/sts/
+echo 'RUN AS: ~/sts/STS'
+echo '--'
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- INSTALLING NetBeans GUI IDE'
+echo '-----------------------------------------------------------------------------------------------'
+
+# The following is a NetBeans standard edition install
+# after installing, use Tools/Plugins; check all in Settings; click Update; Available Plugins Tab
+# .. choose Java EE Base to work with Tomcat
+# .. choose JavaFX 2 Support for some menu shortcuts that maybe works with JavaFX 8 (also adds Maven) -- needs testing
+# .. choose Gluon Plugin for experimental Android / iOS development using Java
+
+apt-get install -y netbeans
+echo '--'
+
+# Todo -- add Intellij IDE
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- ADDING GUI USER WITH SUDO OPTION'
+echo '-----------------------------------------------------------------------------------------------'
+
+# Add a new GUI user with sudo privledges so we can finalize our setup in GUI environment
+
+useradd -m river -c 'River' -s '/bin/bash'
+echo river:mucis | sudo chpasswd
+usermod -aG sudo river
+echo '--'
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- ALL DONE'
+echo '-----------------------------------------------------------------------------------------------'
+
+echo '--'
+echo 'Reboot from AWS control panel.'
+echo 'Use Windows/Mac Remote Desktop to connect to GUI server as user river / mucis.'
+echo 'Finish setup as described in notes above.'
+echo 'Ending shell script at:'
+date
+
+
---
+++ setup.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- UPDATING THE SYSTEM FOR AWS EC2 MULTIUSER STUDENT DEVELOPER UBUNTU 16.04 SERVER'
+echo '---- version 20160820'
+echo '-----------------------------------------------------------------------------------------------'
+
+# This script installs various programming languages, servers, and utilities.
+# Creates a console only version of the server.
+# If you want an xrdp GUI server then run this script followed by the GUI script.
+# I run this as root: $ sudo su -
+# Copy from GitHub raw and paste into vim (i (insert mode) and then right click if using Putty to paste)
+# chmod 700 awscreate.sh
+# ./awscreate.sh
+# takes 3 to 5 minutes to run
+
+echo 'Starting shell script at:'
+date
+whoami
+pwd
+
+# The following line is needed in order to do an oracle java install
+# it must go before an update command. 
+# However, the openjdk seems to be working for JavaFX and so I will comment this out.
+#add-apt-repository ppa:webupd8team/java -y
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- UPDATING THE SERVER'
+echo '-----------------------------------------------------------------------------------------------'
+apt-get -qq update -y
+apt-get -qq upgrade -y
+# could also apt-get -qq dist-upgrade -y  -- not sure if this is needed
+
+echo '---- SETTING HOST NAME'
+hostnamectl set-hostname localhost
+hostnamectl
+
+echo '---- INSTALLING A FEW UTILITY PROGRAMS'
+apt-get -qq install -y git bzip2 zip unzip screen
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- INSTALLING COMPILERS'
+echo '-----------------------------------------------------------------------------------------------'
+
+echo '---- INSTALLING JAVA 8 COMPILER'
+# if using oracle install it asks some interactive questions -- not sure how to automate them
+# back to using openjdk version for now
+apt-get -qq install -y openjdk-8-jdk openjfx
+#apt-get -qq install -y oracle-java8-installer
+javac -version
+java -version
+
+echo '---- INSTALLING JAVA/MYSQL JDBC DRIVER'
+apt-get -qq install libmysql-java
+echo 'CLASSPATH=.:/usr/share/java/mysql-connector-java.jar' >> /etc/environment
+
+echo '----PERL IS PREINSTALLED'
+perl --version
+
+echo '---- PYTHON AND PYTHON3 ARE PREINSTALLED'
+python3 --version
+
+echo '---- RUBY IS PREINSTALLED ON VAGRANT BUT NOT ON AWS'
+apt-get -qq install -y ruby
+ruby --version
+
+echo '---- INSTALLING C AND C++ COMPILERS'
+apt-get -qq install -y build-essential
+gcc --version
+g++ --version
+
+#echo '---- INSTALLING C# COMPILER'
+#apt-get -qq install -y mono-complete
+#mono
+
+#echo '---- INSTALLING GO'
+#apt-get -qq install -y golang
+#go version
+
+#echo '---- INSTALLING CLOJURE'
+#apt-get install -y clojure1.6
+
+#echo '---- INSTALLING LEGACY COMPILERS'
+
+#echo '---- INSTALLING FORTRAN COMPILER'
+#apt-get install -y gfortranq
+#gfortran --version
+
+#echo '---- INSTALLING COBOL COMPILER'
+#apt-get install -y open-cobol
+#cobc -V
+
+#echo '---- INSTALLING PL/I COMPILER'
+#wget http://www.iron-spring.com/pli-0.9.9.tgz
+#tar -xvzf pli-0.9.9.tgz
+#cd pli-0.9.9
+#make install
+#cd ..
+#rm -f pli-0.9.9.tgz
+#plic -V
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- INSTALLING APACHE2, MYSQL, AND PHP7 TO CREATE A LAMP SERVER'
+echo '-----------------------------------------------------------------------------------------------'
+
+apt-get -qq install -y apache2
+echo '--'
+
+echo '---- INSTALLING PHP7'
+apt-get -qq install -y php7.0 php7.0-mysql libapache2-mod-php7.0 php7.0-gd php7.0-json
+echo '--'
+
+echo '---- INSTALLING MYSQL WITH NO ROOT PASSWORD'
+DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
+echo '--'
+
+echo '---- CONFIGURE APACHE2 TO ALLOW CGI AND PUBLIC_HTML USER FOLDERS'
+echo "ServerName localhost" > /etc/apache2/conf-available/servername.conf
+a2enconf servername.conf
+
+sed -i 's/#AddHandler cgi-script .cgi/AddHandler cgi-script .cgi .pl .py .rb/' /etc/apache2/mods-available/mime.conf
+
+sed -i 's/IncludesNoExec/ExecCGI/' /etc/apache2/mods-available/userdir.conf
+
+sed -i 's/<IfModule mod_userdir.c>/#<IfModule mod_userdir.c>/' /etc/apache2/mods-available/php7.0.conf
+sed -i 's/    <Directory/#    <Directory/' /etc/apache2/mods-available/php7.0.conf
+sed -i 's/        php_admin_flag engine Off/#        php_admin_flag engine Off/' /etc/apache2/mods-available/php7.0.conf
+sed -i 's/    <\/Directory>/#    <\/Directory>/' /etc/apache2/mods-available/php7.0.conf
+sed -i 's/<\/IfModule>/#<\/IfModule>/' /etc/apache2/mods-available/php7.0.conf
+
+a2enmod userdir
+a2enmod cgid
+a2disconf serve-cgi-bin
+
+systemctl reload apache2
+systemctl restart apache2
+systemctl status apache2
+
+echo '---- FIXING APACHE ERROR LOG SO ALL USERS CAN READ IT'
+chmod 644 /var/log/apache2/error.log
+chmod 755 /var/log/apache2
+sed -i 's/create 640 root adm/create 644 root adm/' /etc/logrotate.d/apache2
+
+#echo '-----------------------------------------------------------------------------------------------'
+#echo '---- INSTALLING TOMCAT JAVA WEB SERVER'
+#echo '-----------------------------------------------------------------------------------------------'
+# some 16.04 commands for working with tomcat include
+# systemctl status tomcat8
+# systemctl restart tomcat8
+
+#apt-get -qq install -y tomcat8 tomcat8-docs tomcat8-admin tomcat8-examples
+
+#echo '---- CONFIGURE TOMCAT'
+#echo '---- SET THE TOMCAT ADMIN USER: tomcat'
+#echo '---- SET THE TOMCAT ADMIN PASSWORD: tomcatpw'
+#echo '---- UPLOAD AND RUN JSP PROGRAMS AT BROWSER URL OF: ipaddress:8080'
+#sed -i 's/<\/tomcat-users>/  <user username="tomcat" password="mucis" roles="manager-gui,admin-gui"\/><\/tomcat-users>/' /etc/tomcat8/tomcat-users.xml
+
+# Todo -- add a nodejs server option
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- CREATING FILES FOR THE USERS'
+echo '-----------------------------------------------------------------------------------------------'
+
+echo '---- CONFIGURE SKEL WITH TEST FILES FOR ALL USERS'
+mkdir /etc/skel/public_html
+mkdir /etc/skel/public_html/pub2206
+mkdir /etc/skel/public_html/test
+
+echo "<html><body>Hello from HTML</body></html>" > /etc/skel/public_html/test/htmltest.html
+
+echo "<?php phpinfo(); ?>" > /etc/skel/public_html/test/phptest.php
+
+echo "#!/usr/bin/perl" > /etc/skel/public_html/test/perltest.pl
+echo 'print "Content-type: text/html\n\n";' >> /etc/skel/public_html/test/perltest.pl
+echo 'print "Hello from Perl\n";' >> /etc/skel/public_html/test/perltest.pl
+chmod 755 /etc/skel/public_html/test/perltest.pl
+
+echo "#!/usr/bin/python3" > /etc/skel/public_html/test/pythontest.py
+echo 'print ("Content-type: text/html\n\n")' >> /etc/skel/public_html/test/pythontest.py
+echo 'print ("Hello from Python\n")' >> /etc/skel/public_html/test/pythontest.py
+chmod 755 /etc/skel/public_html/test/pythontest.py
+
+echo "#!/usr/bin/ruby" > /etc/skel/public_html/test/rubytest.rb
+echo 'print "Content-type: text/html\n\n"' >> /etc/skel/public_html/test/rubytest.rb
+echo 'print "<html><body><p>Hello using Ruby!</p></body></html>"' >> /etc/skel/public_html/test/rubytest.rb
+chmod 755 /etc/skel/public_html/test/rubytest.rb
+echo '--'
+
+echo "import java.sql.Connection;" > /etc/skel/public_html/test/JDBCTest.java
+echo "import java.sql.DriverManager;" >> /etc/skel/public_html/test/JDBCTest.java
+echo "class JDBCTest {" >> /etc/skel/public_html/test/JDBCTest.java
+echo "public static void main(String[] args) {" >> /etc/skel/public_html/test/JDBCTest.java
+echo '  try(Connection con=DriverManager.getConnection("jdbc:mysql://localhost","yourdbusername","yourdbuserpassword")){' >> /etc/skel/public_html/test/JDBCTest.java
+echo '  System.out.println("Connected");' >> /etc/skel/public_html/test/JDBCTest.java
+echo '} catch (Exception e) {' >> /etc/skel/public_html/test/JDBCTest.java
+echo '  e.printStackTrace();' >> /etc/skel/public_html/test/JDBCTest.java
+echo '}}}' >> /etc/skel/public_html/test/JDBCTest.java
+
+echo '---- ADDING TEST USER jdoe'
+useradd -m jdoe -c 'Jane Doe' -s '/bin/bash'
+
+echo '---- SETTING PASSWORD FOR USER jdoe TO mucis'
+echo jdoe:mucis | sudo chpasswd
+
+echo '---- ALLOWING PASSWORD LOGINS - BE SURE TO SET AWS FIREWALL CORRECTLY TO LIMIT ACCESS BY IP'
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+echo '---- CREATING A TEST DATABASE FOR jdoe'
+mysql -uroot -e "create database jdoe"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON jdoe.* TO jdoe@localhost IDENTIFIED BY 'mucis'"
+mysql -ujdoe -pmucis -e "use jdoe;drop table if exists address;create table address(name varchar(50) not null, street varchar(50) not null, primary key(name));"
+mysql -ujdoe -pmucis -e "use jdoe;insert into address values('Jane', '123 Main Street');insert into address values('Bob', '222 Oak Street');insert into address values('Sue', '555 Trail Street');"
+
+echo '-----------------------------------------------------------------------------------------------'
+echo '--- REMINDERS'
+echo '-----------------------------------------------------------------------------------------------'
+echo '---- Do not forget to set AWS firewall to limit SSH connections to just a few specific ip addresses'
+echo '---- Set firewall to allow port 80 for Apache and port 8080 for Tomcat'
+echo '---- Admin user: ubuntu password: none, log in using AWS private key'
+echo '---- Test user: jdoe password: mucis'
+echo '---- MySQL admin user: root password: none'
+echo '---- MySQL jdoe user: jdoe password: mucis'
+echo '---- Tomcat admin user: tomcat password: mucis'
+echo '---- SECURE MYSQL USING: sudo mysql_secure_installation'
+echo '---- REPLACE THE ABOVE PASSWORDS'
+echo '---- REBOOT THE SERVER FROM THE AWS CONTROL PANEL'
+echo '--'
+echo 'Ending shell script at:'
+date
+
